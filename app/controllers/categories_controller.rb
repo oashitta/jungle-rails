fb9685000eb113661f8ba411db65bb4c76a27808
Omotunde Shitta <103114829+oashitta@users.noreply.github.com>
2023-06-01 21:03:48 -0400

    created categorry actions and routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,37 @@
-class CategoriesController < ApplicationController
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USERNAME"],
+                               password: ENV["HTTP_BASIC_PASSWORD"],
+                               if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
 
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
+  end
+
+  def index
+    @categories = Category.order(:name).all
+  end
+
+  def new
+    @category = Category.new
+  end 
+
+  def create 
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created'
+    else
+      render :new
+  end
+
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 
 end
